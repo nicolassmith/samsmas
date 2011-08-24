64acff613cb8b9981c108a5d3f41d0758cbae97a
nicolassmith <nicolas.d.smith@gmail.com>
2011-08-24 16:01:24 -0400

    calibrate in m/rtHz

--- measscripts/plotworkspace.m
+++ measscripts/plotworkspace.m
@@ -22,22 +22,25 @@ gu = spec(out(:,6));
 noise = spec(out(:,7));
 
 magcal = 0.1;% uT/V
-gurcal = 1/(26*400*2);% (m/s)/V
+gurcal = 1/(26*400*2)./noise.f;% m/V
 
 figure
 subplot(2,1,1)
 loglog(mx.f,mx.x*magcal,'b',my.f,my.x*magcal,'r',mz.f,mz.x*magcal,'g',noise.f,noise.x*magcal,'k')
-axis tight
 legend('X','Y','Z','ADC noise')
 title('Magnetometer')
 xlabel('frequency (Hz)')
-ylabel('Magnetic Field (uT)')
+axis tight
+xlim([min(noise.f) max(noise.f)])
+ylabel('Magnetic Field (uT/\surdHz)')
 
 subplot(2,1,2)
-loglog(gn.f,gn.x*gurcal,'b',ge.f,ge.x*gurcal,'r',gu.f,gu.x*gurcal,'g',noise.f,noise.x*gurcal,'k')
+loglog(gn.f,gn.x.*gurcal,'b',ge.f,ge.x.*gurcal,'r',gu.f,gu.x.*gurcal,'g',noise.f,noise.x.*gurcal,'k')
 axis tight
 legend('NS','EW','UD','ADC Noise')
 title('Guralp')
 xlabel('frequency (Hz)')
-ylabel('Ground Motion (m/s)')
+axis tight
+xlim([min(noise.f) max(noise.f)])
+ylabel('Ground Motion (m/\surdHz)')
 
